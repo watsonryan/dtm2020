12b138ef1df1a751049a9a5b90e322adbf884c48
watsonryan <rwatso12@gmail.com>
2026-02-18 00:27:54 -0700

    enforce Result invariants with variant-backed storage

--- include/dtm2020/result.hpp
+++ include/dtm2020/result.hpp
@@ -3,32 +3,44 @@
 // Author: Watson
 // Purpose: Lightweight C++20 result type for value-or-error returns.
 
-#include <optional>
+#include <cassert>
 #include <utility>
+#include <variant>
 
 namespace dtm2020 {
 
 template <typename T, typename E>
 class Result {
  public:
-  static Result Ok(T value) { return Result(std::move(value), std::nullopt); }
-  static Result Err(E error) { return Result(std::nullopt, std::move(error)); }
+  static Result Ok(T value) { return Result(std::move(value)); }
+  static Result Err(E error) { return Result(std::move(error)); }
 
-  [[nodiscard]] bool has_value() const { return value_.has_value(); }
+  [[nodiscard]] bool has_value() const { return std::holds_alternative<T>(state_); }
   [[nodiscard]] explicit operator bool() const { return has_value(); }
 
-  [[nodiscard]] const T& value() const { return *value_; }
-  [[nodiscard]] T& value() { return *value_; }
+  [[nodiscard]] const T& value() const {
+    assert(has_value());
+    return std::get<T>(state_);
+  }
+  [[nodiscard]] T& value() {
+    assert(has_value());
+    return std::get<T>(state_);
+  }
 
-  [[nodiscard]] const E& error() const { return *error_; }
-  [[nodiscard]] E& error() { return *error_; }
+  [[nodiscard]] const E& error() const {
+    assert(!has_value());
+    return std::get<E>(state_);
+  }
+  [[nodiscard]] E& error() {
+    assert(!has_value());
+    return std::get<E>(state_);
+  }
 
  private:
-  Result(std::optional<T> value, std::optional<E> error)
-      : value_(std::move(value)), error_(std::move(error)) {}
+  explicit Result(T value) : state_(std::move(value)) {}
+  explicit Result(E error) : state_(std::move(error)) {}
 
-  std::optional<T> value_{};
-  std::optional<E> error_{};
+  std::variant<T, E> state_;
 };
 
 }  // namespace dtm2020
